19b00317316ed362c7292631d86cb8b913590005
Vitor Ganiko <vitorseitiganiko@gmail.com>
2026-09-17 23:39:32 -0300

    :green_heart: Chore: remove useState

--- src/pages/paymentRedirect/paymentRedirect.tsx
+++ src/pages/paymentRedirect/paymentRedirect.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { NavLink, useLocation, useSearchParams } from 'react-router-dom';
 
 import OpenInNewRoundedIcon from '@mui/icons-material/OpenInNewRounded';
